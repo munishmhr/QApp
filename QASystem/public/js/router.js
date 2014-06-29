5aa8238ade72bf08254d74dfead37e2f300c5849
roideuniverse <roideuniverse@gmail.com>
2014-06-29 17:53:54 -0400

    Changed indentation and log statements

--- QASystem/public/js/router.js
+++ QASystem/public/js/router.js
@@ -1,39 +1,37 @@
-define(['views/index','models/techCollection'],
-  function(Index,TechCollection) {
-      var QAppRouter = Backbone.Router.extend({
-        currentView: null,
+define([
+	'views/index',
+	'models/techCollection'
+	],
+       
+	function(Index,TechCollection) {
+		var QAppRouter = Backbone.Router.extend({
+			currentView: null,
+			routes: {
+				"index": "index",
+			},
 
-        routes: {
-          "index": "index",
-        },
+			changeView: function(view) {
+				if ( null != this.currentView ) {
+					this.currentView.undelegateEvents();
+				}
+				this.currentView = view;
+				this.currentView.render();
+			},
 
-        changeView: function(view) {
-          if ( null != this.currentView ) {
-            this.currentView.undelegateEvents();
-          }
-
-          this.currentView = view;
-          this.currentView.render();
-        },
-
-        index : function() {
-          var techCollection = new TechCollection();
-          console.log('inside index router.js');
-          var that = this;
-          techCollection.fetch({
-              success : function(){
-                //console.log(techCollection);
-                that.changeView(new Index({
-                    collection : techCollection
-                }));
-              },
-              error : function(){
-                console.log("error");
-              }
-          });
-        },
-
-      });
-
-  return new QAppRouter();
-});
+			index : function() {
+				var techCollection = new TechCollection();
+				console.log('router.js::index');
+				techCollection.fetch({
+					success : function() {
+						console.log('router.js::index::success');
+						that.changeView(new Index( {collection : techCollection} ));
+					},
+					error : function() {
+						console.log("router.js::index::error");
+					}
+				});
+			},
+		});
+		return new QAppRouter();
+	}
+);
