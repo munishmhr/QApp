39fdabca54a85e480c50978e6e5adcc70875fe9e
roideuniverse <roideuniverse@gmail.com>
2014-06-29 16:57:43 -0400

    Fixed indentation

--- QASystem/app.js
+++ QASystem/app.js
@@ -8,52 +8,54 @@ var mongoose = require('mongoose');
 
 // Import the tech table
 var models = {
-  TechnologysList : require('./models/TechnologysList')(mongoose)
+	TechnologysList : require('./models/TechnologysList')(mongoose)
 };
 
-app.configure(function(){
-  app.set('view engine', 'jade');
-  app.use(express.static(__dirname + '/public'));
-  app.use(express.limit('1mb'));
-  app.use(express.bodyParser());
-  app.use(express.cookieParser());
-  app.use(express.session({secret: "QApp", store: new MemoryStore()}));
-  mongoose.connect(dbPath, function onMongooseError(err) {
-    if (err) throw err;
-  });
-});
-
-app.get('/', function(req, res){
-  res.render('index.jade');
-});
-
-app.get('/index',function(req,res){
-	models.TechnologysList.populateTech(function(docs){
-    res.send(docs);
+app.configure(function() {
+	app.set('view engine', 'jade');
+	app.use(express.static(__dirname + '/public'));
+	app.use(express.limit('1mb'));
+	app.use(express.bodyParser());
+	app.use(express.cookieParser());
+	app.use(express.session({secret: "QApp", store: new MemoryStore()}));
+	mongoose.connect(dbPath, function onMongooseError(err) {
+		if (err) throw err;
 	});
 });
 
-app.get('/reviewquestions',function(req,res){
-  models.TechnologysList.question(function(docs){
-    res.send(docs);
-  });
+app.get('/', function(req, res) {
+	res.render('index.jade');
 });
 
-app.post('/questionSubmit',function(req,res){
-  models.TechnologysList.questionInsert(req.param('question',''),req.param('answer',''),
-      req.param('difficultylevel' ),req.param('conceptscore' ),req.param('importancescore')
-      ,function(success){
-          if(success == true){
-            res.send(200);
-            console.log('sucess in inserting question in questionbank');
-          }else{
-            console.log("failed in inserting question in questionbank")
-          }
-        }
-  );
+app.get('/index',function(req,res) {
+	models.TechnologysList.populateTech(function(docs) {
+		res.send(docs);
+	});
+});
 
-  
+app.get('/reviewquestions',function(req,res) {
+	models.TechnologysList.question(function(docs) {
+		res.send(docs);
+	});
+});
+
+app.post('/questionSubmit',function(req,res) {
+	models.TechnologysList.questionInsert(
+		req.param('question',''),
+		req.param('answer',''),
+		req.param('difficultylevel'),
+		req.param('conceptscore'),
+		req.param('importancescore'),
+		function(success) { 
+			if(success == true) {
+				res.send(200);
+				console.log('app.js::post::/questionSubmit:: success'); 
+			} else {
+				console.log("app.js::post::/questionSubmit:: failure");
+			}
+		}
+	);
 });
 
 app.listen(8080);
-console.log("listening to port 8080.");
+console.log("app.js::listening to port 8080.");
