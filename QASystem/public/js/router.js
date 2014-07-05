21292059edc645ac9cf16d6a1f0cd0d192fec0e3
roideuniverse <roideuniverse@gmail.com>
2014-07-05 17:22:02 -0400

    Fixed indentation, added a routes:testIndex for verifying how router works.

--- QASystem/public/js/router.js
+++ QASystem/public/js/router.js
@@ -1,44 +1,53 @@
 /*
-  This is backbone route whenever e.g index (right) is asked in addess index (left)
-  will be invoked. e.g in index method new Index view is invoked with passing a collection
-  to constructor of view.
-*/
-define(['views/index','models/techCollection'],
-  function(Index,TechCollection) {
-      var QAppRouter = Backbone.Router.extend({
-        currentView: null,
-        
-        routes: {
-          "index": "index",
-        },
+ * To Read more about Backbone.Router refer url http://backbonejs.org/#Router
+ *  This is backbone route whenever e.g index (right) is asked in addess index (left)
+ * will be invoked. e.g in index method new Index view is invoked with passing a collection
+ * to constructor of view.
+ */
+define([
+		'views/index',
+		'models/techCollection'
+	],
 
-        changeView: function(view) {
-          if ( null != this.currentView ) {
-            this.currentView.undelegateEvents();
-          }
+	function (Index, TechCollection) {
+		var QAppRouter = Backbone.Router.extend({
+			currentView: null,
 
-          this.currentView = view;
-          this.currentView.render();
-        },
+			routes: {
+				"index": "indexFunction",
+				"testIndex": "testFunction",
+			},
 
-        index : function() {
-          var techCollection = new TechCollection();
-          //console.log('inside index router.js');
-          var that = this;
-          techCollection.fetch({
-              success : function(){
-                //console.log(techCollection);
-                that.changeView(new Index({
-                    collection : techCollection
-                }));
-              },
-              error : function(){
-                console.log("error");
-              }
-          });
-        },
+			changeView: function (view) {
+				console.log("router.js::changeView::");
+				if (null != this.currentView) {
+					this.currentView.undelegateEvents();
+				}
 
-      });
+				this.currentView = view;
+				this.currentView.render();
+			},
 
-  return new QAppRouter();
-});
+			indexFunction: function () {
+				console.log("router.js::indexFunction");
+				var techCollection = new TechCollection();
+				var that = this;
+				techCollection.fetch({
+					success: function () {
+						console.log("router.js::indexFunction::success::");
+						that.changeView(new Index({
+							collection: techCollection
+						}));
+					},
+					error: function () {
+						console.log("router.js::indexFunction::error::");
+					}
+				});
+			},
+
+			testFunction: function () {
+				console.log("router.js::testFunction");
+			}
+		});
+		return new QAppRouter();
+	});
