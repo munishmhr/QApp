6cc8b50bce2d521dd36326248027d33c2f63a8e5
roideuniverse <roideuniverse@gmail.com>
2014-07-05 17:20:23 -0400

    Fixed indentation

--- QASystem/public/js/boot.js
+++ QASystem/public/js/boot.js
@@ -1,20 +1,20 @@
 require.config({
-  paths: {
-    jQuery: '/js/libs/jquery',
-    Underscore: '/js/libs/underscore',
-    Backbone: '/js/libs/backbone',
-    text: '/js/libs/text',
-    templates: '../templates',
-    bootstrap: '/js/libs/bootstrap'
-  },
+	paths: {
+		jQuery: '/js/libs/jquery',
+		Underscore: '/js/libs/underscore',
+		Backbone: '/js/libs/backbone',
+		text: '/js/libs/text',
+		templates: '../templates',
+		bootstrap: '/js/libs/bootstrap'
+	},
 
-  shim: {
-    'Backbone': ['Underscore', 'jQuery'],
-    'QApp': ['Backbone']
-  }
+	shim: {
+		'Backbone': ['Underscore', 'jQuery'],
+		'QApp': ['Backbone']
+	}
 });
 
-require(['QApp'], function(QApp) {
-  //console.log("inside boot");
-  QApp.initialize();
+require(['QApp'], function (QApp) {
+	//console.log("inside boot");
+	QApp.initialize();
 });
