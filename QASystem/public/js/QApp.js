18fb1fa7b413439e7e9ccd4582f6daea900033dc
roideuniverse <roideuniverse@gmail.com>
2014-07-05 17:19:46 -0400

    Fixed indentation

--- QASystem/public/js/QApp.js
+++ QASystem/public/js/QApp.js
@@ -2,19 +2,18 @@
   This class is called in boot file as redirecting to view which should be represent to user
   when "/" address is asked.
 */
+define(['router'], function (router) {
+	var initialize = function () {
+		bootup();
+	};
 
-define(['router'], function(router) {
-  var initialize = function() {
-    bootup();
-  };
+	var bootup = function () {
+		window.location.hash = 'index';
+		Backbone.history.start();
+	}
 
-  var bootup = function(){
-   	window.location.hash = 'index';
-   	Backbone.history.start();
-  }
-  
-  return {
-    initialize : initialize
-  };
+	return {
+		initialize: initialize
+	};
 
 });
